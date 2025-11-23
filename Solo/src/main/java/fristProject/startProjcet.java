ca160ec67b20aa5950ce4aab74b2a37c3455dfcd
dodos <dodos@192.168.219.106>
2025-11-23 15:41:21 +0900

    2025/11/23 LEE
    서블릿 페이지
    HTML문서와 분리

--- Solo/src/main/java/fristProject/startProjcet.java
+++ Solo/src/main/java/fristProject/startProjcet.java
@@ -1,7 +1,45 @@
 package fristProject;
 
-public class startProjcet {
-	public static void main(String[] args) {
-		System.out.println("start JAVA");
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/main.do")
+public class startProjcet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		RequestDispatcher rd = req.getRequestDispatcher("number1Pro.html");
+		rd.forward(req, resp);
+		
+		String name = req.getParameter("name");
+		
+		PrintWriter pw = resp.getWriter();
+		
+		
+		
+		
+		pw.print("<html>");
+		pw.print("<body>");
+		pw.print("<a>"+name+"</a>");
+		pw.print("</body>");
+		pw.print("</html>");
+		
+		
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+	}
+	
 }
